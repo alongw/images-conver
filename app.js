381cc5de5ce7f0b812209b8a9333bf722b5dd755
alongw <alongw@alongw.cn>
2024-02-14 22:35:47 +0800

    feat: upload file

---
+++ app.js
@@ -0,0 +1,38 @@
+import shell from 'shelljs'
+import fse from 'fs-extra'
+
+/**
+ * @typedef {Object} Config
+ * @property {string} ffmpegPath
+ * @property {string} inputDir
+ * @property {string} outputDir
+ * @property {string[]} inputType
+ * @property {number} maxWidth
+ * @property {number} outType
+ * @property {string} outName
+ * @property {number} quality
+ */
+
+/** @type {Config} */
+const config = fse.readJsonSync('./config.json')
+
+fse.ensureDirSync(config.inputDir)
+fse.ensureDirSync(config.outputDir)
+
+const fileList = shell.ls(`${config.inputDir}/*.+(${config.inputType.join('|')})`)
+
+fileList.forEach((e, i) => {
+    const fileName = e.split('/').pop() // image.jpg
+    const liteLame = fileName.split('.').shift() // image
+    const outName = config.outName.replace('{name}', liteLame) // {name}.[config.outType]
+    const output = `${config.outputDir}/${outName}.${config.outType}`
+    shell.exec(
+        `${config.ffmpegPath} -y -i "${e}" -q:v "${config.quality}" -vf "scale=${config.maxWidth}:-1" "${output}"`,
+        { silent: true }
+    )
+    console.log(
+        `处理第${i + 1}个文件(${fileName} => ${outName}.${config.outType})，共${
+            fileList.length
+        }个文件`
+    )
+})
